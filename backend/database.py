bd9cfd9ff2e042b69f309cb3f25e6912df5dbefd
Opiattu <kknoss2004@gmail.com>
2026-03-26 03:41:13 +0300

    setproduction api url

--- backend/database.py
+++ backend/database.py
@@ -7,8 +7,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL не задан")
 
-if DATABASE_URL.startswith("https://eckxvbayzgasuugeuphe.supabase.co"):
-    DATABASE_URL = DATABASE_URL.replace("https://eckxvbayzgasuugeuphe.supabase.co", "https://eckxvbayzgasuugeuphe.supabase.co", 1)
+if DATABASE_URL.startswith("https://sspsctopwksoqebfmwtj.supabase.co"):
+    DATABASE_URL = DATABASE_URL.replace("https://sspsctopwksoqebfmwtj.supabase.co", "https://sspsctopwksoqebfmwtj.supabase.co", 1)
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
